e582886ff92ced8fd3df96518843a2a08f3ff270
khufkens <koen.hufkens@gmail.com>
2022-01-10 19:11:19 +0100

    CI fix

--- tests/testthat/test_icoscp.r
+++ tests/testthat/test_icoscp.r
@@ -3,6 +3,10 @@ context("test icoscp queries")
 test_that("station checks",{
   skip_on_cran()
 
+  # libraries (fail to dynamically load -- issue with SPARQL?)
+  library(RCurl)
+  library(SPARQL)
+
   # download the data
   expect_type(
     icos_stations(),
