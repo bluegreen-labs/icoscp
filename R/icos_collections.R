c063612f150f8d5c3506b7643705311adeecf310
khufkens <koen.hufkens@gmail.com>
2022-01-10 19:56:35 +0100

    collections

--- R/icos_collections.R
+++ R/icos_collections.R
@@ -10,40 +10,46 @@
 
 icos_collections <- function(id) {
 
-  # def collections(id=None):
-  #   """
-  #   Return Collections
-  #   Parameters
-  #   ----------
-  #   id : STR, optional
-  #       The default is None, which returns all know collections.
-  #       You can provide a ICOS URI or DOI to filter for a specifict collection
-  #   Returns
-  #   -------
-  #   query : STR
-  #       A query, which can be run against the SPARQL endpoint.
-  #   """
-  #
-  # if not id:
-  #   coll = '' # create an empyt string insert into sparql query
-  #   else:
-  #     coll = ''.join(['FILTER(str(?collection) = "' + id+ '" || ?doi = "' + id + '") .'])
-  #
-  #     query = """
-  #           prefix cpmeta: <http://meta.icos-cp.eu/ontologies/cpmeta/>
-  #           prefix dcterms: <http://purl.org/dc/terms/>
-  #           select * where{
-  #           ?collection a cpmeta:Collection .
-  #           %s
-  #           OPTIONAL{?collection cpmeta:hasDoi ?doi} .
-  #           ?collection dcterms:title ?title .
-  #           OPTIONAL{?collection dcterms:description ?description}
-  #           FILTER NOT EXISTS {[] cpmeta:isNextVersionOf ?collection}
-  #           }
-  #           order by ?title
-  #           """ % coll
-  #
-  #     return query
+  # define endpoint
+  endpoint <- server()
 
+  # add station filter
+  if(!missing(station)){
+    flt <- sprintf("FILTER(?collection = '%s' || ?doi = '%s' ) . ",id, id)
+  } else {
+    flt <- ""
+  }
+
+  # define query
+  query <- sprintf("
+           prefix cpmeta: <http://meta.icos-cp.eu/ontologies/cpmeta/>
+             prefix dcterms: <http://purl.org/dc/terms/>
+             select * where{
+               ?collection a cpmeta:Collection .
+               %s
+               OPTIONAL{?collection cpmeta:hasDoi ?doi} .
+               ?collection dcterms:title ?title .
+               OPTIONAL{?collection dcterms:description ?description}
+               FILTER NOT EXISTS {[] cpmeta:isNextVersionOf ?collection}
+             }
+           order by ?title
+            ", flt)
+
+  # retrieve sparql data
+  df <- try(
+    SPARQL::SPARQL(
+      endpoint,
+      query,
+      format = "xml"
+    )$results
+  )
+
+  # check results
+  if(inherits(df, "try-error") || nrow(df) == 0 ) {
+    stop("No data returned, check your id name")
+  }
+
+  # return results as a dataframe
+  print(df)
 
 }
