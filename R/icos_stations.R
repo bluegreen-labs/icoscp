f7a5b81c7dd623dd26f15784d627f5872ca15056
khufkens <koen.hufkens@gmail.com>
2022-01-12 11:51:31 +0100

    full station list

--- R/icos_stations.R
+++ R/icos_stations.R
@@ -12,36 +12,24 @@
 #' all_station_info <- icos_stations()
 #' }
 
-icos_stations <- function(station) {
+icos_stations <- function(
+  station
+  ) {
 
   # define endpoint
   endpoint <- server()
 
   # add station filter
   if(!missing(station)){
-    flt <- sprintf("FILTER(?stationId = '%s' ) . ",station)
+    flt_icos <- sprintf("FILTER(?stationId = '%s' ) . ",station)
+    flt_full <- sprintf("FILTER(?id = '%s' ) . ",station)
   } else {
-    flt <- ""
+    flt_icos <- ""
+    flt_full <- ""
   }
 
   # define query
-  # query <- sprintf("
-  #           prefix cpmeta: <http://meta.icos-cp.eu/ontologies/cpmeta/>
-  #           select *
-  #           from <http://meta.icos-cp.eu/resources/icos/>
-  #           from <http://meta.icos-cp.eu/resources/extrastations/>
-  #           where {
-  #           	?uri cpmeta:hasStationId ?id .
-  #           	%s
-  #           	OPTIONAL {?uri cpmeta:hasName ?name  } .
-  #           	OPTIONAL {?uri cpmeta:countryCode ?country }.
-  #           	OPTIONAL {?uri cpmeta:hasLatitude ?lat }.
-  #           	OPTIONAL {?uri cpmeta:hasLongitude ?lon }.
-  #           	OPTIONAL {?uri cpmeta:hasElevation ?elevation } .
-  #           }
-  #           ", flt)
-
-  query <- sprintf('
+  icos_query <- sprintf('
             prefix st: <http://meta.icos-cp.eu/ontologies/stationentry/>
             select distinct ?stationId ?stationName ?stationTheme
             ?class  ?siteType
@@ -66,22 +54,71 @@ icos_stations <- function(station) {
 
                 BIND (replace(str(?stationClass), "http://meta.icos-cp.eu/ontologies/stationentry/", "") AS ?stationTheme )
             }
-        ', flt)
+        ', flt_icos)
 
+  full_query <- sprintf("
+            prefix cpmeta: <http://meta.icos-cp.eu/ontologies/cpmeta/>
+            select *
+            from <http://meta.icos-cp.eu/resources/icos/>
+            from <http://meta.icos-cp.eu/resources/extrastations/>
+            where {
+            	?uri cpmeta:hasStationId ?id .
+            	%s
+            	OPTIONAL {?uri cpmeta:hasName ?name  } .
+            	OPTIONAL {?uri cpmeta:countryCode ?country }.
+            	OPTIONAL {?uri cpmeta:hasLatitude ?lat }.
+            	OPTIONAL {?uri cpmeta:hasLongitude ?lon }.
+            	OPTIONAL {?uri cpmeta:hasElevation ?elevation } .
+            }
+            ", flt_full)
 
   # retrieve sparql data
-  df <- try(
+  # for well documented icos sites
+  icos_df <- try(
     SPARQL::SPARQL(
         endpoint,
-        query,
+        icos_query,
         format = "xml"
       )$results
     )
 
+  # retrieve sparql data
+  # for all sites (regardless of meta-data quality)
+  full_df <- try(
+    SPARQL::SPARQL(
+      endpoint,
+      full_query,
+      format = "xml"
+    )$results
+  )
+
   # check results
-  if(inherits(df, "try-error") || nrow(df) == 0 ) {
+  if(inherits(icos_df, "try-error") || nrow(icos_df) == 0 ) {
     stop("No data returned, check your station name")
   }
+
+  # check results
+  if(inherits(full_df, "try-error") || nrow(full_df) == 0 ) {
+    stop("No data returned, check your station name")
+  }
+
+  # merge two datasets by station id
+  icos_df <- dplyr::rename(icos_df,
+      'id' = 'stationId',
+      'name' = 'stationName',
+      'theme' = 'stationTheme',
+      'type' = 'siteType',
+      'first_name' = 'firstName',
+      'last_name' = 'lastName'
+    ) |>
+    dplyr::select(
+      -lat,
+      -lon,
+      -country
+    )
+
+  # join the data
+  df <- dplyr::left_join(full_df, icos_df, by = c('id','name'))
 
   # return results as a dataframe
   return(df)
--- analysis/test_query.R
+++ analysis/test_query.R
@@ -2,8 +2,18 @@ library(SPARQL)
 
 source("R/zzz.R")
 source("R/icos_stations.R")
+source("R/icos_station_data.R")
 
-
-test <- icos_stations(station = "FR-Tou")
+test <- icos_stations("FR-Tou")
 
 print(head(test))
+#
+# library(tidyverse)
+#
+# bla <- test %>%
+#   filter(
+#    id == "FR-Tou"
+#   ) %>%
+#   select(uri)
+#
+# icos_station_data('http://meta.icos-cp.eu/resources/stations/ES_FR-Tou')
