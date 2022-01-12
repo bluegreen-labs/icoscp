bacc9f2a74a79e1b040b477551c7e49e41c037e2
khufkens <koen.hufkens@gmail.com>
2022-01-12 21:26:13 +0100

    logo

--- logo/logo.R
+++ logo/logo.R
@@ -1,15 +1,16 @@
 library(hexSticker)
 library(tidyverse)
 
-imgurl <- "logo/1280px-Carbon_dioxide_3D_ball.png"
+imgurl <- "logo/dots.png"
 
 sticker(imgurl,
         package = "icoscp",
         p_size = 40,
-        p_color = "black",
-        h_color = "black",
-        h_fill = "white",
-        filename="logo.png",
-        s_y = 1.5,
-        p_y = 1
+        p_color = "white",
+        h_color = "#e41c64",
+        h_fill = "#2596be",
+        filename = "logo.png",
+        s_y = 1.3,
+        s_x = 1,
+        p_y = 0.9
         )
